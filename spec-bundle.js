95587ed5172850bcfac55a3c1914e39f232015e7
Гортинский Дмитрий <dgortinsky@rutube.ru>
2016-02-02 18:57:04 +0300

    небольшие исправления

--- spec-bundle.js
+++ spec-bundle.js
@@ -20,7 +20,7 @@ require('zone.js/dist/jasmine-patch.js');
 
 
 var testing = require('angular2/testing');
-var browser = require('angular2/platform/testing/browser.js');
+var browser = require('angular2/platform/testing/browser');
 testing.setBaseTestProviders(
     browser.TEST_BROWSER_PLATFORM_PROVIDERS,
     browser.TEST_BROWSER_APPLICATION_PROVIDERS);

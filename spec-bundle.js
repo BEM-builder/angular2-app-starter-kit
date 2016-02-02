328a7c05c6b6fa497b6009a91cc75357ff777639
Гортинский Дмитрий <dgortinsky@rutube.ru>
2016-02-02 18:54:34 +0300

    unit тесты, исправления

--- spec-bundle.js
+++ spec-bundle.js
@@ -7,7 +7,7 @@
  * file for webpack test. Just like webpack will create a bundle.js
  * file for our client, when we run test, it well compile and bundle them
  * all here! Crazy huh. So we need to do some setup
-*/
+ */
 Error.stackTraceLimit = Infinity;
 require('phantomjs-polyfill');
 require('es6-promise');
@@ -18,24 +18,25 @@ require('zone.js/lib/browser/zone-microtask.js');
 require('zone.js/lib/browser/long-stack-trace-zone.js');
 require('zone.js/dist/jasmine-patch.js');
 
+
+var testing = require('angular2/testing');
+var browser = require('angular2/platform/testing/browser.js');
+testing.setBaseTestProviders(
+    browser.TEST_BROWSER_PLATFORM_PROVIDERS,
+    browser.TEST_BROWSER_APPLICATION_PROVIDERS);
+
 /*
-  Ok, this is kinda crazy. We can use the the context method on
-  require that webpack created in order to tell webpack
-  what files we actually want to require or import.
-  Below, context will be an function/object with file names as keys.
-  using that regex we are saying look in ./src/app and ./test then find
-  any file that ends with spec.js and get its path. By passing in true
-  we say do this recursively
-*/
+ Ok, this is kinda crazy. We can use the the context method on
+ require that webpack created in order to tell webpack
+ what files we actually want to require or import.
+ Below, context will be an function/object with file names as keys.
+ using that regex we are saying look in ./src/app and ./test then find
+ any file that ends with spec.js and get its path. By passing in true
+ we say do this recursively
+ */
 var testContext = require.context('./', true, /\.spec\.ts/);
 
 // get all the files, for each file, call the context function
 // that will require the file and load it up here. Context will
 // loop and require those spec files here
 testContext.keys().forEach(testContext);
-
-// Select BrowserDomAdapter.
-// see https://github.com/AngularClass/angular2-webpack-starter/issues/124
-var domAdapter = require('angular2/src/platform/browser/browser_adapter');
-domAdapter.BrowserDomAdapter.makeCurrent();
-

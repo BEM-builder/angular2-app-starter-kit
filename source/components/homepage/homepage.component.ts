ae7bdaa8ea4103f0648545853e1648e97a3c157e
awibox <wibox@ya.ru>
2016-02-03 17:05:24 +0300

    scss

--- source/components/homepage/homepage.component.ts
+++ source/components/homepage/homepage.component.ts
@@ -3,18 +3,12 @@ import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
 import {FirstPageComponent} from './../../components/firstpage/firstpage.component';
 import {SecondPageComponent} from './../../components/secondpage/secondpage.component';
+import {Menu} from './../menu/menu'
 
 @Component({
     selector: 'home-component',
-    template: `
-    <h1>Test app home page</h1>
-    <nav>
-      <a [routerLink]="['FirstPage']">Player page</a>
-      <a [routerLink]="['SecondPage']">Forms page</a>
-      <a [routerLink]="['HomePage']">HomePage</a>
-    </nav>
-    `,
-    directives: ROUTER_DIRECTIVES
+    template: require('./homepage.template.html'),
+    directives: [ROUTER_DIRECTIVES, Menu]
 })
 
 export class HomeComponent {

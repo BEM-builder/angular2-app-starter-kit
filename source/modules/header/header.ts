59f193395827b50c4b6c49eccdfd3c0653150b1e
Гортинский Дмитрий <dgortinsky@rutube.ru>
2016-02-04 14:40:52 +0300

    routerLink && router-link-active

--- source/modules/header/header.ts
+++ source/modules/header/header.ts
@@ -1,11 +1,13 @@
 import {Component, Input} from 'angular2/core'
 import {Menu} from './../menu/menu'
 
+import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
 @Component({
     selector: 'header',
     template: require('./header.html'),
     styles: [ require('./header.scss').toString() ],
-    directives: [Menu]
+    directives: [Menu, ROUTER_DIRECTIVES]
 })
 
 export class Header {
--- source/modules/menu/menu.ts
+++ source/modules/menu/menu.ts
@@ -1,16 +1,23 @@
 import {Data} from '../../data/data'
 
 import {Component, Input} from 'angular2/core'
+import {Router, RouteConfig, RouterLink, ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
     selector: 'menu',
     template: require('./menu.template.html'),
-    styles: [ require('./menu.scss').toString() ]
+    styles: [ require('./menu.scss').toString()],
+    directives: [RouterLink]
     //providers: [Data]
 })
 
 export class Menu {
     @Input() modificator : string;
+    private routeNames : Array<string> = [
+        'FirstPage',
+        'SecondPage',
+        'HomePage'
+    ];
     private data : any = new Data().getData();
     constructor() {
 

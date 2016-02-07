63d4a90259e50942d4277cd04425852d6ec6b43a
awibox <wibox@ya.ru>
2016-02-08 00:56:49 +0300

    Страница с формами

--- source/app/pages/secondpage/secondpage.ts
+++ source/app/pages/secondpage/secondpage.ts
@@ -1,49 +1,15 @@
 import {Component} from 'angular2/core';
 import {NgForm} from 'angular2/common';
+import {Form} from './../../../modules/form/form';
 
 @Component({
     selector: 'secondpage',
     template: require('./secondpage.html'),
+    directives: [Form]
 })
 
 export class SecondPageComponent {
-
-    private firstFormSubmitCounter : number = 0;
-    private secondFormData : string = '';
-    private model : Object = {
-        name : 'Somename',
-        surname : 'Somesurname'
-    };
-    private selectable : Array<string> = [
-        'firstItem', 'secondItem', 'thirdItem'
-    ];
-
     constructor() {
 
-    }
-
-    getFirstFormSubmitCounter() {
-        return this.firstFormSubmitCounter;
-    }
-
-    onFirstFormSubmit() {
-        this.firstFormSubmitCounter++;
-    }
-
-    getSecondFormData() {
-        return this.secondFormData;
-    }
-
-    onSecondFormSubmit() {
-        let selectable : any = document.getElementById('selectable');
-        this.secondFormData = selectable[selectable.selectedIndex].text;
-    }
-
-    getModel() {
-        return JSON.stringify(this.model);
-    }
-
-    getSelectables() {
-        return this.selectable.toString();
     }
 }

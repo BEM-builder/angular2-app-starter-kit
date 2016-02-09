6901d79b353f08ba9cfa995fbde3275d23b72d42
Andrey Arkhipov <aarkhipov@rutube.ru>
2016-02-09 15:10:39 +0300

    directives для дефолтного модуля

--- source/build/module/default.ts
+++ source/build/module/default.ts
@@ -3,7 +3,8 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'default',
     template: require('./default.html'),
-    styles: [ require('./default.scss').toString()]
+    styles: [ require('./default.scss').toString()],
+    //directives: []
 })
 
 export class Default {
